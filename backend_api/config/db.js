053a399e9aa80c76380ecce49236b185ebfb577d
eswart627 <eswartai@gmail.com>
2026-09-01 02:52:40 +0530

    debug: db error

--- backend_api/config/db.js
+++ backend_api/config/db.js
@@ -11,13 +11,6 @@ const sequelize = new Sequelize(
     dialect: "mysql",
     logging: false,
     timezone: "+00:00",
-
-    dialectOptions: {
-      ssl: {
-        ca: process.env.MYSQL_SSL_CA,
-        rejectUnauthorized: true,
-      },
-    },
   },
 );
 
