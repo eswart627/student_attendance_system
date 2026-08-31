e46a67581653b5cee5b894cb69f4415fabb6d523
eswart627 <eswartai@gmail.com>
2026-09-01 01:37:02 +0530

    port problem in sql

--- backend_api/config/db.js
+++ backend_api/config/db.js
@@ -7,10 +7,11 @@ const sequelize = new Sequelize(
   process.env.MYSQL_PASSWORD,
   {
     host: process.env.MYSQL_HOST,
+    port: Number(process.env.MYSQL_PORT),
     dialect: "mysql",
     logging: false,
     timezone: "+00:00",
-  }
+  },
 );
 
 sequelize
