2db6ba10c1feef131bd010435da491816e7563b9
eswart627 <eswartai@gmail.com>
2026-09-01 18:03:52 +0530

    debug: teacher_frontend fix

--- teacher_app/src/lib/axios.js
+++ teacher_app/src/lib/axios.js
@@ -1,12 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: "https://student-attendance-system-kr95.onrender.com/api",
   headers: { "Content-Type": "application/json" },
 });
 
 export default api;
-
-
-// $2b$10$/QfuCsWOxgwrwj0UoLvbHuynSEeCmeOpdn2C8LsjzfIw6a7uUMyDm
-// $2b$10$/QfuCsWOxgwrwj0UoLvbHuynSEeCmeOpdn2C8LsjzfIw6a7uUMyDm
